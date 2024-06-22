d18deef0aced21f35b4fbda5a4a70603ae4ac232
philipuuu <topgun0415@naver.com>
2024-06-22 19:17:07 +0900

    🔧 Fix : Layout & Color & Logo fixed

--- blog-site/src/login/LoginPage.jsx
+++ blog-site/src/login/LoginPage.jsx
@@ -1,42 +1,13 @@
 import React, { useState } from 'react';
-import Header from '@/components/Header';
-import { useRouter } from 'next/router';
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const router = useRouter();
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const res = await fetch('/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email, password }),
-    });
-
-    const data = await res.json();
-    if (res.ok) {
-      alert('Login successful: ' + JSON.stringify(data));
-      // セッションストレージにデータを保存
-      // ここではPostテーブルの情報全てをsetする予定(IDでUserテーブルと紐付ける感じ、？)
-      sessionStorage.setItem('postData', JSON.stringify(data));
-      router.push('/bloglist');
-    } else {
-      alert('Login failed: ' + data.message);
-    }
-  };
-
   return (
     <>
       <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
         <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
           <img
-            className='mx-auto h-10 w-auto'
-            src='https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600'
+            className='object-contain h-48 w-96'
+            src='/logo.jpg'
             alt='Your Company'
           />
           <h2 className='mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900'>
@@ -45,70 +16,65 @@ const LoginPage = () => {
         </div>
 
         <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
-          <form className='space-y-6' onSubmit={handleSubmit}>
-            <div>
+          <div className='mb-3'>
+            <label
+              htmlFor='email'
+              className='block text-sm font-medium leading-6 text-gray-900'>
+              Email address
+            </label>
+            <div className='mt-2'>
+              <input
+                id='email'
+                name='email'
+                type='email'
+                autoComplete='email'
+                required
+                className='block w-full rounded-md border-0 py-1.5 text-black shadow-sm ring-1 ring-inset ring-gray-300 sm:text-sm sm:leading-6 placeholder:italic placeholder:text-slate-400'
+                placeholder='myblog@example.com'
+              />
+            </div>
+          </div>
+
+          <div>
+            <div className='flex items-center justify-between'>
               <label
-                htmlFor='email'
+                htmlFor='password'
                 className='block text-sm font-medium leading-6 text-gray-900'>
-                Email address
+                Password
               </label>
-              <div className='mt-2'>
-                <input
-                  id='email'
-                  name='email'
-                  type='email'
-                  autoComplete='email'
-                  required
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                />
+              <div className='text-sm'>
+                <a
+                  href='#'
+                  className='font-semibold text-orange-600 hover:text-orange-500'>
+                  Forgot password?
+                </a>
               </div>
             </div>
-
-            <div>
-              <div className='flex items-center justify-between'>
-                <label
-                  htmlFor='password'
-                  className='block text-sm font-medium leading-6 text-gray-900'>
-                  Password
-                </label>
-                <div className='text-sm'>
-                  <a
-                    href='#'
-                    className='font-semibold text-indigo-600 hover:text-indigo-500'>
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-              <div className='mt-2'>
-                <input
-                  id='password'
-                  name='password'
-                  type='password'
-                  autoComplete='current-password'
-                  required
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                  className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
-                />
-              </div>
+            <div className='mt-2'>
+              <input
+                id='password'
+                name='password'
+                type='password'
+                autoComplete='current-password'
+                required
+                className='block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+              />
             </div>
+          </div>
 
-            <div>
-              <button
-                type='submit'
-                className='flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-                Login
-              </button>
-            </div>
-          </form>
+          <div>
+            <button
+              type='submit'
+              className='flex w-full justify-center mt-5 rounded-md bg-orange-400 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+              Login
+            </button>
+          </div>
 
-          <p className='mt-10 text-center text-sm text-gray-500'>
+          <p className='mt-10 text-center text-sm text-black'>
             Not a member?{' '}
             <a
               href='#'
-              className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'>
+              className='font-semibold leading-6 text-orange-600 hover:text-orange-500'>
               Join our blogger
             </a>
           </p>
