60e0984aa93cfc6dfcd4e3b5c8b2e153d244b06d
philipuuu <topgun0415@naver.com>
2024-06-22 19:07:13 +0900

    Config : tailwind CSS apply range added

--- blog-site/tailwind.config.js
+++ blog-site/tailwind.config.js
@@ -3,12 +3,10 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {},
-    },
+    extend: {},
   },
   plugins: [],
 };
